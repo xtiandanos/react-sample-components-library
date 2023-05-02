09b2d99fde5dde220bfb4f9996c559b394999f67
Christian V. Danos <cris.danos@Christians-MacBook-Pro.local>
2023-05-02 09:32:14 +0800

    application of ReactReportsViewer Component

---
+++ dist/esm/components/ReactReportsViewer/ReactReportsViewer.d.ts
@@ -0,0 +1,34 @@
+/// <reference types="react" />
+interface ReportTypeList {
+    displayValue: string;
+    id: number;
+    referenceCode: string;
+    type: string;
+    validUntilDate: string;
+}
+interface ReportNameList {
+    id: string;
+    value: string;
+}
+interface RCCodeList {
+    id: string;
+    value: string;
+}
+interface InitialValues {
+    reportType: string;
+    reportName: string;
+    rCCode: string;
+}
+interface Props {
+    handleSubmit?: () => void;
+    imageReportPathname?: string;
+    initialValues: InitialValues;
+    isLoading: boolean;
+    rCCodeList?: RCCodeList[];
+    reportNameList?: ReportNameList[];
+    reportTypeList?: ReportTypeList[];
+    setInitialValues: (args: InitialValues) => void;
+}
+export declare const getDataUrl: (blob: BlobPart) => string;
+declare const ReactReportsViewer: ({ handleSubmit, imageReportPathname, initialValues, isLoading, rCCodeList, reportNameList, reportTypeList, setInitialValues }: Props) => JSX.Element;
+export default ReactReportsViewer;
