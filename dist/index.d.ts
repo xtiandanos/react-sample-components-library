3cfa51db5b163fe2c4269fa8d4ed46163cc3fc67
Christian V. Danos <cris.danos@Christians-MacBook-Pro.local>
2023-05-02 14:57:02 +0800

    update ReactReportsViewer component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,8 +4,36 @@ interface ButtonProps {
 }
 declare const Button: (props: ButtonProps) => JSX.Element;
 
-interface Props {
+interface ReportTypeList {
+    displayValue: string;
+    id: number;
+    referenceCode: string;
+    type: string;
+    validUntilDate: string;
 }
-declare const ReactReportsViewer: ({}: Props) => JSX.Element;
+interface ReportNameList {
+    id: string;
+    value: string;
+}
+interface RCCodeList {
+    id: string;
+    value: string;
+}
+interface InitialValues {
+    reportType: string;
+    reportName: string;
+    rCCode: string;
+}
+interface Props {
+    handleSubmit: () => void;
+    imageReportPathname?: string;
+    initialValues: InitialValues;
+    isLoading: boolean;
+    rCCodeList?: RCCodeList[];
+    reportNameList?: ReportNameList[];
+    reportTypeList?: ReportTypeList[];
+    setInitialValues: (args: InitialValues) => void;
+}
+declare const ReactReportsViewer: ({ handleSubmit, imageReportPathname, initialValues, isLoading, rCCodeList, reportNameList, reportTypeList, setInitialValues }: Props) => JSX.Element;
 
 export { Button, ReactReportsViewer };
